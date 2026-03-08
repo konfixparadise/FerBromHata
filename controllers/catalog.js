1ada79bbea041b6388a9687436efb775196bb371
pc <20113217@setu.ie>
2026-03-08 15:55:58 +0000

    Views are conntected to controllers

--- controllers/catalog.js
+++ controllers/catalog.js
@@ -2,29 +2,15 @@
 
 import logger from "../utils/logger.js"
 
-const cataloges = [
-  {
-    id: 1,
-    title: "Piano Sonata No. 3",
-    artist: "Beethoven"
-  },
-  {
-    id: 2,
-    title: "Piano Sonata No. 7",
-    artist: "Beethoven"
-  },
-  {
-    id: 3,
-    title: "Piano Sonata No. 10",
-    artist: "Beethoven"
-  }
-];
-
 const catalog = {
   createView(request, response) {
     logger.info("Catalog page loading!")
-    logger.debug("Loading the catalog", cataloges)
-    response.json(cataloges);   
+    
+    const viewData = {
+      title: 'FerBromHata App Catalog'
+    }
+
+    response.render('catalog', viewData)
   }
 }
 
--- controllers/start.js
+++ controllers/start.js
@@ -5,11 +5,14 @@ import appStore from "../models/app-store.js"
 
 const start = {
     createView(request, response) {
-        const info = appStore.getAppInfo();
-        logger.debug(info)
-
         logger.info("Start page loading!")
-        response.json(info)
+        
+        const viewData = {
+            title: "Welcome to the FerBromHata app!",
+            info: appStore.getAppInfo()
+        }
+
+        response.render('start', viewData)
     }
 }
 
