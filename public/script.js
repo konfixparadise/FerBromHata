15e0ec2ed0f8a343ea8ceff3755bd100cdcbb24e
pc <20113217@setu.ie>
2026-03-13 01:24:48 +0000

    Menu fully operatable, mobile filter menu version is done

--- public/script.js
+++ public/script.js
@@ -1,1 +1,21 @@
-console.log("Hello from the Web App!");
+document.addEventListener('DOMContentLoaded', () => {
+    if (window.location.pathname === '/catalog'){
+        const filterContainer = document.getElementById('filter_header_container')
+        const filterMenu = document.getElementById('filter_menu')
+        const icon = filterContainer.querySelector('.filter_icon_btn')
+
+        filterContainer.addEventListener('click', function () {
+            if (window.innerWidth <= 800) {
+                filterMenu.classList.toggle('filter_open')
+
+                if (filterMenu.classList.contains('filter_open')) {
+                    icon.classList.remove('down')
+                    icon.classList.add('up')
+                } else {
+                    icon.classList.remove('up')
+                    icon.classList.add('down')
+                }
+            }
+        })
+    }
+})
